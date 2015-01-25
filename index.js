dd481e6f867d5ba14e514b1e817e4c6cbc876661
Chris Truman <cleetruman@gmail.com>
2015-01-24 22:52:53 -0800

    Testing movies endpoint

--- index.js
+++ index.js
@@ -475,7 +475,7 @@ Showtimes.prototype.getMovies = function(mid, cb) {
 
         // Use the hidden API of getTheaters to pass in the next page and current
         // theaters.
-        self.getMovies(cb, ++page, movies);
+        self.getTheaters(cb, ++page, movies);
 
         return;
     });
